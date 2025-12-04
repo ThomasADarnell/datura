b0ea03a31f6682ff081bdf6796260184a2db8fd9
ThomasADarnell <155779405+ThomasADarnell@users.noreply.github.com>
2025-12-03 21:20:00 -0800

    Added credits (Tyler)

--- Assets/Scripts/Manager/LevelManager.cs
+++ Assets/Scripts/Manager/LevelManager.cs
@@ -5,7 +5,22 @@ public class LevelManager : MonoBehaviour
 {
     public static string lastLevelName = "Menu"; // Default to Main Menu scene
 
-    void Start() { }
+    [Header("Level 2 Win Screen Images")]
+    public GameObject level2WinImage1;
+    public GameObject level2WinImage2;
+
+    void Start() 
+    {
+        // Check if we're on the Win Screen after completing Level 2
+        if (SceneManager.GetActiveScene().name == "Win Screen" && lastLevelName == "2D Level 2")
+        {
+            EnableLevel2WinImages();
+        }
+        else
+        {
+            DisableLevel2WinImages();
+        }
+    }
     void Update() { }
 
     public void LoadLevel(string levelName)
@@ -30,5 +45,45 @@ public class LevelManager : MonoBehaviour
     public void LevelTwo()
     {
         LoadLevel("2D Level 2");
+    }
+
+    public void MainMenu()
+    {
+        LoadLevel("Menu");
+    }
+
+    public void NextLevel()
+    {
+        // After Level 2, go back to main menu
+        if (lastLevelName == "2D Level 2")
+        {
+            MainMenu();
+        }
+        // After Level 1, go to Level 2
+        else if (lastLevelName == "Lvl1" || lastLevelName == "Lvl1-3d")
+        {
+            LevelTwo();
+        }
+        // Default: go to Level 1
+        else
+        {
+            LevelOne();
+        }
+    }
+
+    private void EnableLevel2WinImages()
+    {
+        if (level2WinImage1 != null)
+            level2WinImage1.SetActive(true);
+        if (level2WinImage2 != null)
+            level2WinImage2.SetActive(true);
+    }
+
+    private void DisableLevel2WinImages()
+    {
+        if (level2WinImage1 != null)
+            level2WinImage1.SetActive(false);
+        if (level2WinImage2 != null)
+            level2WinImage2.SetActive(false);
     }
 }
